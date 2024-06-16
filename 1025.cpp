bf31b134b8722f9f561dcb82b75fe3d065565eaf
katle <1619752019@qq.com>
2024-06-16 23:51:59 +0800

    init

---
+++ 1025.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//int ans=0;
+int w[1001];
+int dp[100][200];
+vector<int>s; 
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	for(int i=1;i<=n;i++)//每一个数最多允许由k个数相加得来 
+	{
+		
+	}
+	//cout<<ans<<endl;
+	return 0;
+}
